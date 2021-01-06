9e5946260e91a67f6b74cc773de90b46332c32a3
hamcheeseburger <yoo77hyeon@gmail.com>
2021-01-06 14:16:31 +0900

    git lfs

--- realTime.py
+++ realTime.py
@@ -9,31 +9,7 @@ import os
 import simpleaudio as sa
 import realTime_main
 
-# 푸쉬용 커밋
-global face_recog
-
-
-class Thread(QThread):
-    changePixmap = pyqtSignal(QImage)
-    scaled_size = QSize(640, 480)
-
-    def run(self):
-        global face_recog
-        face_recog = realTime_main.FaceRecog()
-        while True:
-            frame = face_recog.get_frame()
-            if frame is not None:
-                # print(frame.shape)
-                rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0],
-                                           QImage.Format_RGB888)
-                p = convertToQtFormat.scaled(self.scaled_size, Qt.KeepAspectRatio)
-                self.changePixmap.emit(p)
-
-    def scaled(self, scaled_size):
-        self.scaled_size = scaled_size
-
-
+# 커밋용
 class ExecuteRealTime(RealTimeUi):
     def __init__(self, id):
         RealTimeUi.__init__(self)
@@ -61,26 +37,94 @@ class ExecuteRealTime(RealTimeUi):
         self.btn_sound_start.clicked.connect(self.sound_handler)
         # self.btn_select_route.clicked.connect(self.select_route)
 
+        # 시작 버튼 미클릭시 버튼 클릭 못하게
+        self.btn_cam_start.setDisabled(True)
+        self.btn_sound_start.setDisabled(True)
+        # self.btn_start.setDisabled(True)
+        self.btn_end.setDisabled(True)
+
         self.show()
 
-    @pyqtSlot(QImage)
-    def changePixmapHandler(self, image):
-        # self.videoLabel.setPixmap(QPixmap.fromImage(image))
-        if self.isCameraDisplayed is True:
-            # 실시간이기 때문에 비디오 크기가 아님 웹캠 사이즈로 고정
-            self.videoLabel.setFixedSize(640, 480)
-            self.videoLabel.setPixmap(QPixmap.fromImage(image))
-            self.adjustSize()
+
 
     # 시작버튼 눌렸을 때 실행되는 함수
     def start_recog(self):
-        self.print_total_working.setText("근무시간 측정 중..")
+        self.face_recog = realTime_main.FaceRecog()
+        self.print_total_working.setText("근무시간 측정중..")
+        self.btn_start.setDisabled(True)
 
-        self.th1 = Thread(self)
-        self.th1.changePixmap.connect(self.changePixmapHandler)
-        # print(self.face_recog.known_face_names)
-        # 프레임 추출하는 과정에 대해서만 쓰레드 시작 그 이후 코드는 쓰레드 핸들러에서 실행
-        self.th1.start()
+        start = time.time()
+        print(start)
+        # 캠onoff, 소리onoff, 종료 버튼 활성화
+        self.btn_cam_start.setDisabled(False)
+        self.btn_sound_start.setDisabled(False)
+        self.btn_end.setDisabled(False)
+        while True:
+            frame = self.face_recog.get_frame()
+            # frame이 아니라 jpg byte를 받아와서 이미지 출력
+            # bytes -> QPixmap으로 변환하는 것이 핵심
+            # frame이 null이 아닐 경우에만 윈도우 상 출력
+            if frame is None:
+                print("frame is None")
+                break
+            if frame is not None and self.isCameraDisplayed is True:
+                # print('frame is not None')
+                # 매개 변수 jpg_bytes -> 비디오 파일 꺼지자 마자 프로그램 강제 종료
+                # 매개 변수 face_recog.get_jpg_bytes() -> 이상 없음
+                byte = self.face_recog.get_jpg_bytes()
+                if byte is not None:
+                    self.my_bytes = QByteArray(byte)
+                    self.bytes_pixmap = QPixmap()
+                    ok = self.bytes_pixmap.loadFromData(self.my_bytes)
+                    assert ok
+                    self.videoLabel.setPixmap(self.bytes_pixmap)
+                    # self.videoLabel.setFixedSize(1280, 720)
+                    self.videoLabel.setFixedSize(self.bytes_pixmap.size())
+                    self.videoLabel.adjustSize()
+                    self.adjustSize()
+
+            # 근무 신호등 교체
+            self.isWorking = self.face_recog.working
+            if self.isWorking is True:
+                self.change_traffic_light("./templates/Traffic_Lights_green.png")
+                if self.workingAlarmFlag is False and self.alarmMute is False:
+                    # print('working alarm!!')
+                    play_obj = self.workingWav.play()
+                    # play_obj.wait_done()
+                    self.workingAlarmFlag = True
+                    self.slackOffAlarmFlag = False
+            else:
+                self.change_traffic_light("./templates/Traffic_Lights_red.png")
+                if self.slackOffAlarmFlag is False and self.alarmMute is False:
+                    # print('slackOff alarm!!')
+                    play_obj = self.notWorkingWav.play()
+                    self.slackOffAlarmFlag = True
+                    self.workingAlarmFlag = False
+
+            if self.stopFlag or self.face_recog.video_end:
+                break
+            cv2.waitKey(5) & 0xFF
+            self.face_recog.notifyIsPaused(self.pauseFlag)
+        end = time.time()
+        print(str(end) + ", " + str(end - start))
+        cv2.destroyAllWindows()
+
+        self.print_total_working.setText(self.face_recog.calculate_total())
+        print("finish")
+        # 종료버튼을 누르고 나서 신호등과 카메라 화면을 초기화
+        self.isCameraDisplayed = False
+        self.videoLabel.setText("근무종료")
+        self.videoLabel.setFixedSize(100, 30)
+        # self.videoLabel.resize(self.videoLabel.width(), self.videoLabel.height())
+        self.change_traffic_light("./templates/Traffic_Lights_init.png")
+        # 윈도우 창을 적절하게 자동으로 조정
+        self.setFixedSize(400, 200)
+        self.btn_sound_start.setDisabled(True)
+        self.btn_cam_start.setDisabled(True)
+        self.btn_end.setDisabled(True)
+        self.btn_start.setDisabled(False)
+        self.adjustSize()
+        # self.resize(400, 200)
 
     def change_traffic_light(self, file_path):
         self.pixmap = QPixmap(self.scriptDir + os.path.sep + file_path)
@@ -102,13 +146,17 @@ class ExecuteRealTime(RealTimeUi):
 
     def cam_handler(self):
         if self.isCameraDisplayed is True:
+            print('cam_off')
+            self.btn_cam_start.setText('Camera On')
             self.isCameraDisplayed = False
             self.videoLabel.setText('화면 중지')
             self.videoLabel.setFixedSize(100, 30)
-            # self.setFixedSize(400, 300)
+            self.setFixedSize(400, 200)
             self.adjustSize()
+            # self.resize(400, 200)
         else:
             print('cam_start')
+            self.btn_cam_start.setText('Camera Off')
             self.isCameraDisplayed = True
             # 실시간이기 때문에 비디오 크기가 아님 웹캠 사이즈로 고정
             # self.videoLabel.setFixedSize(1280, 720)
@@ -116,8 +164,10 @@ class ExecuteRealTime(RealTimeUi):
     def sound_handler(self):
         # print("사운드 핸들러 동작")
         if self.alarmMute is False:
+            self.btn_sound_start.setText('Sound On')
             self.alarmMute = True
         else:
+            self.btn_sound_start.setText('Sound Off')
             self.alarmMute = False
 
 
